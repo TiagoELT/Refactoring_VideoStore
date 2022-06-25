88519029277909cb422053157613a35d3eddc06f
Tiago Rezende <43640757+TiagoELT@users.noreply.github.com>
2022-06-25 19:59:16 +0000

    Commit 15

--- Price.java
+++ Price.java
@@ -8,11 +8,6 @@ public abstract class Price {
   public abstract double getCharge(int daysRented);
 
    public int getFrequentRenterPoints(int daysRented) {
-     //Adicionar o trecho de código extraído.
-    _frequentRenterPoints ++;
-    // add bonus for a two day new release rental
-        if ((_each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-            _each.getDaysRented() > 1) _frequentRenterPoints ++;
-    return _frequentRenterPoints;    
+     return 1;    
    }
 }
