5ba6e7caa2cdff3ba4ca6ce32ad0064ad6661a82
Tiago Rezende <43640757+TiagoELT@users.noreply.github.com>
2022-06-25 19:55:47 +0000

    Commit 14

--- Movie.java
+++ Movie.java
@@ -6,8 +6,7 @@ public class Movie {
 
   private String _title;
   private Price _price;
-  private Rental _each;
-  private int _frequentRenterPoints = 0;
+  
 
   public Movie(String title, int priceCode) {
       _title = title;
@@ -42,12 +41,7 @@ public class Movie {
     return _price.getCharge(daysRented);    
    }
 
-  public int getFrequentRenterPoints(int daysRented){
-    //Adicionar o trecho de código extraído.
-    _frequentRenterPoints ++;
-    // add bonus for a two day new release rental
-        if ((_each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-            _each.getDaysRented() > 1) _frequentRenterPoints ++;
-    return _frequentRenterPoints;
-   }  
+  public int getFrequentRenterPoints(int daysRented) {
+      return _price.getFrequentRenterPoints(daysRented);
+   }
 }
--- Price.java
+++ Price.java
@@ -1,9 +1,18 @@
 public abstract class Price {
   
   private Rental _each;
+  private int _frequentRenterPoints = 0;
   
   public abstract int getPriceCode();
 
   public abstract double getCharge(int daysRented);
-  
+
+   public int getFrequentRenterPoints(int daysRented) {
+     //Adicionar o trecho de código extraído.
+    _frequentRenterPoints ++;
+    // add bonus for a two day new release rental
+        if ((_each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+            _each.getDaysRented() > 1) _frequentRenterPoints ++;
+    return _frequentRenterPoints;    
+   }
 }
