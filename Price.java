d1b7fa2994486f38d45058e54649a1d59392c1fc
Tiago Rezende <43640757+TiagoELT@users.noreply.github.com>
2022-06-25 19:52:35 +0000

    Commit 13

--- Price.java
+++ Price.java
@@ -4,24 +4,6 @@ public abstract class Price {
   
   public abstract int getPriceCode();
 
-  public double getCharge(int daysRented) {
-    double thisAmount = 0;
-    //determine amounts for each line
-        switch (_each.getMovie().getPriceCode()) {
-           case Movie.REGULAR:
-              thisAmount += 2;
-              if (_each.getDaysRented() > 2)
-                 thisAmount += (_each.getDaysRented() - 2) * 1.5;
-              break;
-           case Movie.NEW_RELEASE:
-              thisAmount += _each.getDaysRented() * 3;
-              break;
-           case Movie.CHILDRENS:
-              thisAmount += 1.5;
-              if (_each.getDaysRented() > 3)
-                 thisAmount += (_each.getDaysRented() - 3) * 1.5;
-               break;
-        }
-    return thisAmount;
-   }
+  public abstract double getCharge(int daysRented);
+  
 }
